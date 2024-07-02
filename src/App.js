ba029863a10607660216c80f4f6fdf43a487634e
Armoni Tigner <atigner2@student.gsu.edu>
2024-07-02 14:51:12 -0400

    MSF-3: added background image for landing page and moore savory logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import LandingPage from './Components/LandingPage/landingPage';
+import ResponsiveAppBar from './Components/materialUI NAVbar/navBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          DRZ Commit
-        </a>
-      </header>
-    </div>
+    <><ResponsiveAppBar /><LandingPage /></>
   );
 }
 
