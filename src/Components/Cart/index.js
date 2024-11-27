e3d1f9fb5867c7f2dec9b2b57d764814748fd5d1
Armoni Tigner <atigner2@student.gsu.edu>
2024-11-26 22:58:06 -0500

    sending orders from cart to backend working

--- src/Components/Cart/index.js
+++ src/Components/Cart/index.js
@@ -3,13 +3,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import axios from "axios";
 
 
 const Cart = ({saladsAddedToCart, setSaladsAddedToCart, total, setTotal }) => {
 
 const grabSalads = JSON.parse(sessionStorage.getItem('cart'))
 
-
+console.log(grabSalads, 'yoo')
 const removeSalad = (key) => {
     const items = JSON.parse(sessionStorage.getItem('cart'))
     items.splice(key,1)
@@ -21,6 +22,27 @@ const removeSalad = (key) => {
     setTotal('0.00')
 
 
+}
+const placeOrder =  async () => {
+    const z = {salad: 'Armoni', total: 9, toppings: 'toppin'}
+    console.log(z.salad, z.total, z.toppings)
+    grabSalads[0]['name'] = 'Armoni'
+    console.log(grabSalads, 'grabem')
+
+    for(let key in grabSalads){
+       
+        grabSalads[key]['name'] = 'Armoni'
+    }
+
+
+    try{
+      await  axios.post("http://localhost:8800/customerOrder", grabSalads )
+        console.log('hi')
+        setTotal('0.00')
+    }
+    catch(err){
+        console.error(err)
+    }
 }
     return (
         <React.Fragment>
@@ -42,7 +64,7 @@ const removeSalad = (key) => {
                 })
             }
             <h1>Total: </h1>
-    <Button variant="contained">Place Order</Button>
+    <Button variant="contained" onClick={() => placeOrder()}>Place Order</Button>
 </Box>
 
         </Container>
